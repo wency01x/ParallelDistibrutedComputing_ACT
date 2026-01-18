72a3d60c116e57959be97015a64ef08997fef654
Harvie Babuyo <hlcbabuyo@gmail.com>
2026-01-18 02:12:14 +0000

    Harvie: Added subtract function

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,15 @@
 def add(x, y):
     return x + y
 
+def subtract(x, y):
+    return x - y
+
 def parse_number(value):
     return float(value) if '.' in value else int(value)
 
 print("Select operation:")
 print("1. Add")
+print("2. Subtract")
 
 while True:
     choice = input("Enter choice(1/2/3/4): ")
@@ -17,9 +21,18 @@ while True:
         result = add(num1, num2)
         print(num1, "+", num2, "=", result)
 
+    elif choice == '2':
+        num1 = parse_number(input("Enter first number: "))
+        num2 = parse_number(input("Enter second number: "))
+        result = subtract(num1, num2)
+        print(num1, "-", num2, "=", result)
+
         next_calculation = input("Let's do next calculation? (y/n): ")
         if next_calculation.lower() == 'y':
             continue
         else:
             print("Exiting the calculator. Goodbye!")
             break
+
+    
+
