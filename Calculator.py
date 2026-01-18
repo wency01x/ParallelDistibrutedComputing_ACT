ac49f2af617688d194759d03c6419c7af4f724bb
Wency Casino <wencywego21@gmail.com>
2026-01-18 02:40:35 +0000

    Wency Casino: Added divide function

--- Calculator.py
+++ Calculator.py
@@ -7,6 +7,10 @@ def subtract(x, y):
 def multiply(x, y):
     return x * y
 
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    return x / y
 
 def parse_number(value):
     return float(value) if '.' in value else int(value)
@@ -15,6 +19,7 @@ print("Select operation:")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
+print("4. Divide")
 
 while True:
     choice = input("Enter choice(1/2/3/4): ")
@@ -37,6 +42,12 @@ while True:
         num2 = parse_number(input("Enter second number: "))
         result = multiply(num1, num2)
         print(num1, "*", num2, "=", result)
+
+    elif choice == '4':
+        num1 = parse_number(input("Enter first number: "))
+        num2 = parse_number(input("Enter second number: "))
+        result = divide(num1, num2)
+        print(num1, "/", num2, "=", result)
 
         next_calculation = input("Let's do next calculation? (y/n): ")
         if next_calculation.lower() == 'y':
