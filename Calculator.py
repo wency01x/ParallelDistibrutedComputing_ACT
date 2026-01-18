eb494542c8f1b8817c3ae0a916d08e4964d1cbea
Kerby <kerbyfabriabual@gmail.com>
2026-01-18 02:26:37 +0000

    Kerby: Added Multiply Function

--- Calculator.py
+++ Calculator.py
@@ -4,12 +4,17 @@ def add(x, y):
 def subtract(x, y):
     return x - y
 
+def multiply(x, y):
+    return x * y
+
+
 def parse_number(value):
     return float(value) if '.' in value else int(value)
 
 print("Select operation:")
 print("1. Add")
 print("2. Subtract")
+print("3. Multiply")
 
 while True:
     choice = input("Enter choice(1/2/3/4): ")
@@ -26,6 +31,12 @@ while True:
         num2 = parse_number(input("Enter second number: "))
         result = subtract(num1, num2)
         print(num1, "-", num2, "=", result)
+
+    elif choice == '3':
+        num1 = parse_number(input("Enter first number: "))
+        num2 = parse_number(input("Enter second number: "))
+        result = multiply(num1, num2)
+        print(num1, "*", num2, "=", result)
 
         next_calculation = input("Let's do next calculation? (y/n): ")
         if next_calculation.lower() == 'y':
